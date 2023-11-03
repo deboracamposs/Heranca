1ae41f1f56c7ee7efd0502ed87f7696ee259122a
Débora Campos <127258657+deboracamposs@users.noreply.github.com>
2023-11-02 22:49:17 -0300

    Add files via upload

---
+++ bibliotecahrç.py
@@ -0,0 +1,30 @@
+class Animal:
+    def __init__(self, nome, cor):
+        self.nome=nome
+        self.cor=cor
+    def comer(self):
+        print(f'{self.nome} está comendo!')
+
+class Gato(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome,cor)
+    def emitirsom(self):
+        print(f'🐈 {self.nome} está miando... MIAU')
+
+class Cachorro(Animal):
+    def __int__(self, nome, cor):
+        super().__init__(nome, cor)
+    def emitirsom(self):
+        print(f'🐕 {self.nome} está latindo... AUAU')
+
+class Coelho(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+    def emitirsom(self):
+        print(f'🐇 {self.nome} está ronronando... ronrom')
+
+class Vaca(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+    def emitirsom(self):
+        print(f'🐄 {self.nome} está mugindo... MUUUUU')
---
+++ herança.py
@@ -0,0 +1,26 @@
+from bibliotecahrç import *
+print('BEM VINDO A FAZENDINHA DO CDD \n🌾🌾🌾🏠🌾🌾🌾')
+print('O que os animais estão fazendo?\n')
+
+gato=Gato('Luis', 'Preto' )
+gato.emitirsom()
+gato.comer()
+print()
+
+cachorro=Cachorro('Pietra', 'caramelo')
+cachorro.emitirsom()
+cachorro.comer()
+print()
+
+coelho=Coelho('Jorge', 'branquinho')
+coelho.emitirsom()
+coelho.comer()
+print()
+
+vaca=Vaca('Mimosa', 'malhada')
+vaca.emitirsom()
+vaca.comer()
+print()
+
+
+
